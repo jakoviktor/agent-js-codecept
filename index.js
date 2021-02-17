1d73b96b7c2cf0c970caac4b90e3c8ea874b2874
viktor.jako <viktor.jako@automizy.com>
2021-02-17 15:26:14 +0100

    package.json fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const RPClient = require('client-javascript');
+const RPClient = require('@reportportal/client-javascript');
 const fs = require('fs');
 const path = require('path');
 const debug = require('debug')('codeceptjs:reportportal');
